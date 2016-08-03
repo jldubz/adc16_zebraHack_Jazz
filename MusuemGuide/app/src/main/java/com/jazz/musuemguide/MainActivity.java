b0bbf9c25cd5917c6be586b999586bfa8c02945a
Jon-Luke West <jon-luke.west@opticalphusion.com>
2016-08-02 21:01:59 -0400

    roll together

--- MusuemGuide/app/src/main/java/com/jazz/musuemguide/MainActivity.java
+++ MusuemGuide/app/src/main/java/com/jazz/musuemguide/MainActivity.java
@@ -1,15 +1,72 @@
 package com.jazz.musuemguide;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.FloatMath;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager sensorMan;
+    private Sensor accelerometer;
+
+    private float[] mGravity;
+    private float mAccel;
+    private float mAccelCurrent;
+    private float mAccelLast;
+
+    boolean bStill = false;
+
+    private BroadcastReceiver dataWedgeIntentReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent i) {
+
+            // get the source of the data
+            String source = i.getStringExtra("com.motorolasolutions.emdk.datawedge.source");
+            if (source == null) source = "scanner";
+
+            // get the data from the intent
+            final String data = i.getStringExtra("com.motorolasolutions.emdk.datawedge.data_string");
+
+            //get the type
+            String barcodeType = i.getStringExtra("com.motorolasolutions.emdk.datawedge.label_type");
+
+            // check if the data has come from the barcode scanner
+            if (source.equalsIgnoreCase("scanner")) {
+                // check if there is anything in the data
+                if (data != null && data.length() > 0) {
+
+
+                    final TextView tvHello = (TextView) findViewById(R.id.tvHello);
+                    tvHello.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            tvHello.setText(data);
+                        }
+                    });
+                    View rootView = (View) findViewById(android.R.id.content);
+                    Snackbar.make(rootView, data, Snackbar.LENGTH_LONG)
+                            .show();
+                    //barcodeScannedListener.onBarcodeScanned(data,barcodeType);
+                }
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +83,29 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        sensorMan = (SensorManager)getSystemService(SENSOR_SERVICE);
+        accelerometer = sensorMan.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mAccel = 0.00f;
+        mAccelCurrent = SensorManager.GRAVITY_EARTH;
+        mAccelLast = SensorManager.GRAVITY_EARTH;
+
+        IntentFilter filter = new IntentFilter("com.jazz.musuemguide.RECVR");
+        filter.addCategory("android.intent.category.DEFAULT");
+        registerReceiver(dataWedgeIntentReceiver, filter);
+    }
+
+    @Override
+    protected void onResume() {
+        sensorMan.registerListener(this, accelerometer,
+                SensorManager.SENSOR_DELAY_UI);
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        sensorMan.unregisterListener(this);
+        super.onPause();
     }
 
     @Override
@@ -48,5 +128,50 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        /*if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
+            mGravity = event.values.clone();
+            // Shake detection
+            float x = mGravity[0];
+            float y = mGravity[1];
+            float z = mGravity[2];
+            mAccelLast = mAccelCurrent;
+            mAccelCurrent = (float) Math.sqrt(x*x + y*y + z*z);
+            float delta = mAccelCurrent - mAccelLast;
+            mAccel = mAccel * 0.9f + delta;
+            // Make this higher or lower according to how much
+            // motion you want to detect
+            Log.d("LOGTAG", String.valueOf(mAccel));
+            if(mAccel < 0.1){
+                if (!bStill) {
+                    bStill = true;
+                    Intent i = new Intent();
+                    i.setAction("com.symbol.datawedge.api.ACTION_SOFTSCANTRIGGER");
+                    i.putExtra("com.symbol.datawedge.api.EXTRA_PARAMETER", "START_SCANNING");
+                    sendBroadcast(i);
+                }
+                *//*View rootView = (View) findViewById(android.R.id.content);
+                Snackbar.make(rootView, "Enable Scanner", Snackbar.LENGTH_LONG)
+                        .show();*//*
+                // do something
+
+            } else {
+                bStill = false;
+                Intent i = new Intent();
+                i.setAction("com.symbol.datawedge.api.ACTION_SOFTSCANTRIGGER");
+                i.putExtra("com.symbol.datawedge.api.EXTRA_PARAMETER", "STOP_SCANNING");
+                sendBroadcast(i);
+                //Disable
+            }
+        }*/
+
     }
 }
